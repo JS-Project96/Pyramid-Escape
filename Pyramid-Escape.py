0c0695e555f50bc4c0c99f6d4457769e74482c08
JS-Project96 <jshorter206854@gmail.com>
2023-03-20 12:29:43 +0000

    Added Enemy + Boss class, __init__ + __repr__

--- Pyramid-Escape.py
+++ Pyramid-Escape.py
@@ -13,8 +13,30 @@ class Hero:
     def __repr__(self):
         return self.name, self.health, self.strength, self.spellpower, self.defence
 
+# hero = Hero()
+# print(hero.__repr__())
 
 
+class Enemy:
+    
+    def __init__(self, name, health, strength, spellpower, defence):
+        self.name = name
+        self.health = health
+        self.strength = strength
+        self.spellpower = spellpower
+        self.defence = defence
 
-hero = Hero()
-print(hero.__repr__())
+    def __repr__(self):
+        return self.name, self.health, self.strength, self.spellpower, self.defence
+
+class Boss:
+    def __init__(self, name, health, health_regen, strength, spellpower, defence):
+        self.name = name
+        self.health = health
+        self.health_regen = health_regen
+        self.strength = strength
+        self.spellpower = spellpower
+        self.defence = defence
+
+    def __repr__(self) -> str:
+        return self.name, self.health, self.health_regen, self.strength, self.spellpower, self.defence

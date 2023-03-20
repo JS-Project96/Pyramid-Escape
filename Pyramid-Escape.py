a5fd7fce3c8364dbf9e5e13cb0fde968c8aec274
JS-Project96 <jshorter206854@gmail.com>
2023-03-20 12:22:50 +0000

    Added Hero Class, __init__, __repr__ + testing

--- Pyramid-Escape.py
+++ Pyramid-Escape.py
@@ -1,1 +1,20 @@
 
+# Classes
+
+class Hero:
+
+    def __init__(self, name = "Dave", health = 250, strength = 40, spellpower = 0, defence = 20):
+        self.name = name
+        self.health = health
+        self.strength = strength
+        self.spellpower = spellpower
+        self.defence = defence
+    
+    def __repr__(self):
+        return self.name, self.health, self.strength, self.spellpower, self.defence
+
+
+
+
+hero = Hero()
+print(hero.__repr__())

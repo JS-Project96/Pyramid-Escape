4682621f6de7dedc3ae085d0b5abfe51d36e3bce
JS-Project96 <jshorter206854@gmail.com>
2023-03-20 12:45:43 +0000

    Added basic methods to hero class. Lose_health, attack, cast spell

--- Pyramid-Escape.py
+++ Pyramid-Escape.py
@@ -12,6 +12,24 @@ class Hero:
     
     def __repr__(self):
         return self.name, self.health, self.strength, self.spellpower, self.defence
+    
+    def lose_health(self, amount):
+        self.health = self.health - (amount - self.defence)
+        print("{name} has {health} health remaining".format(name = self.name, health = self.health))
+
+    def attack(self, enemy):
+        damage_dealt = self.strength - enemy.defence
+        print("{name} attacks {enemy} for {damage} damage!".format(name = self.name, enemy = enemy.name, damage = damage_dealt))
+        enemy.lose_health(self.strength)
+        if enemy.health <= 0:
+            print("{enemy} has been defeated! Well done!".format(enemy = enemy.name))
+    
+    def cast_spell(self, enemy):
+        damage_dealt = self.spellpower - (enemy.defence * 3/4)
+        print("{name}'s spell hits {enemy} for {damage} damage!".format(name = self.name, enemy = enemy.name, damage = damage_dealt))
+        enemy.lose_health(self.strength)
+        if enemy.health <= 0:
+            print("{enemy} has been defeated! Well done!".format(enemy = enemy.name))
 
 # hero = Hero()
 # print(hero.__repr__())

7c01e67d6d5692d7c5e22391abfe0587ea1b48ce
JS-Project96 <jshorter206854@gmail.com>
2023-03-20 12:57:10 +0000

    Added basic methods to enemy, boss class. Added regen_health method to boss

--- Pyramid-Escape.py
+++ Pyramid-Escape.py
@@ -25,7 +25,7 @@ class Hero:
             print("{enemy} has been defeated! Well done!".format(enemy = enemy.name))
     
     def cast_spell(self, enemy):
-        damage_dealt = self.spellpower - (enemy.defence * 3/4)
+        damage_dealt = self.spellpower - ((enemy.defence * 3)/4)
         print("{name}'s spell hits {enemy} for {damage} damage!".format(name = self.name, enemy = enemy.name, damage = damage_dealt))
         enemy.lose_health(self.strength)
         if enemy.health <= 0:
@@ -46,6 +46,24 @@ class Enemy:
 
     def __repr__(self):
         return self.name, self.health, self.strength, self.spellpower, self.defence
+    
+    def lose_health(self, amount):
+        self.health = self.health - (amount - self.defence)
+        print("{name} has {health} health remaining".format(name = self.name, health = self.health))
+
+    def attack(self, hero):
+        damage_dealt = self.strength - hero.defence
+        print("{name} attacks {hero} for {damage} damage!".format(name = self.name, hero = hero.name, damage = damage_dealt))
+        hero.lose_health(self.strength)
+        if hero.health <= 0:
+            print("{hero} has been defeated! Try again?".format(hero = hero.name))
+    
+    def cast_spell(self, hero):
+        damage_dealt = self.spellpower - ((hero.defence * 3)/4)
+        print("{name}'s spell hits {hero} for {damage} damage!".format(name = self.name, hero = hero.name, damage = damage_dealt))
+        hero.lose_health(self.strength)
+        if hero.health <= 0:
+            print("{hero} has been defeated! Try again?".format(hero = hero.name))
 
 class Boss:
     def __init__(self, name, health, health_regen, strength, spellpower, defence):
@@ -58,3 +76,24 @@ class Boss:
 
     def __repr__(self) -> str:
         return self.name, self.health, self.health_regen, self.strength, self.spellpower, self.defence
+    
+    def lose_health(self, amount):
+        self.health = self.health - (amount - self.defence)
+        print("{name} has {health} health remaining".format(name = self.name, health = self.health))
+    
+    def regen_health(self):
+        health += self.health_regen
+
+    def attack(self, hero):
+        damage_dealt = self.strength - hero.defence
+        print("{name} attacks {hero} for {damage} damage!".format(name = self.name, hero = hero.name, damage = damage_dealt))
+        hero.lose_health(self.strength)
+        if hero.health <= 0:
+            print("{hero} has been defeated! Try again?".format(hero = hero.name))
+    
+    def cast_spell(self, hero):
+        damage_dealt = self.spellpower - ((hero.defence * 3)/4)
+        print("{name}'s spell hits {hero} for {damage} damage!".format(name = self.name, hero = hero.name, damage = damage_dealt))
+        hero.lose_health(self.strength)
+        if hero.health <= 0:
+            print("{hero} has been defeated! Try again?".format(hero = hero.name))
